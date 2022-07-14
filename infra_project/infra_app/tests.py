21472bb256fdc4f17ee40e803a5a76e26473964b
sapphirehead <djey36@gmail.com>
2022-07-14 18:03:15 +0300

    after test fix

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -13,6 +13,7 @@ class StaticPagesURLTests(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         response = self.guest_client.get('/second_page/')
+        print('Responce1: ', response)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
@@ -21,4 +22,5 @@ class StaticPagesURLTests(TestCase):
         self.assertContains(response, 'У меня получилось!')
 
         response = self.guest_client.get('/second_page/')
+        print('Responce2: ', response)
         self.assertContains(response, 'А это вторая страница!')
